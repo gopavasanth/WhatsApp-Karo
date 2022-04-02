da6c8ebd65516ae3335de8b6d98e2d69a31fee6c
Gopa <vasanth.gopa@sap.com>
2022-04-02 23:10:32 +0530

    Updates to v0.5

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@
 import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() {
@@ -27,99 +28,126 @@ class Home extends StatelessWidget {
     return Scaffold(
         backgroundColor: Colors.white,
         resizeToAvoidBottomInset: false,
-        body:
-        Center(
-          child: Column(
-            children: <Widget>[
-              Expanded(child:
-              Container(
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage("assets/WhatsAppKaro2.jpg"),
-                    fit: BoxFit.cover,
-                  ),
-                ),
-                child:
-                Padding(
-                  padding: EdgeInsets.fromLTRB(30, 200, 30, 0),
-                  child:
-                  Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      Text(
-                        "Send messages without saving contacts!",
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                          color: Colors.black,
-                          fontSize: 20.0,
-                          fontFamily: 'Roboto Mono sample',
-                        ),
-                      ),
-                      SizedBox(height: 20),
-                      TextField(
-                        controller: number,
-                        keyboardType: TextInputType.number,
-                        textAlignVertical: TextAlignVertical.center,
-                        cursorColor: Theme.of(context).primaryColor,
-                        inputFormatters: <TextInputFormatter>[
-                          FilteringTextInputFormatter.digitsOnly,
-                          LengthLimitingTextInputFormatter(10)
-                        ],
-                        decoration: InputDecoration(
-                            suffixIcon: IconButton(
-                              onPressed: (){
-                                number.clear();
-                              },
-                              icon: Icon(
-                                Icons.clear,
-                                color: Colors.black,
-                              ),
-                            ),
-                            focusedBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black, width: 1.0),
-                            ),
-                            enabledBorder: OutlineInputBorder(
-                              borderSide: BorderSide(color: Colors.black, width: 1.0),
-                            ),
-                            hintText: 'Mobile Number',
-                            border: InputBorder.none,
-                            contentPadding: EdgeInsets.all(10.0),
-                            prefixIcon: Padding(
-                                padding: EdgeInsets.all(15),
-                                child: Text('+91 ')
-                            ),
-                        ),
-                      ),
-                      SizedBox(height: 10),
-                      RaisedButton(
-                          onPressed: () => _launchURLBrowser(number.text),
-                          color: Colors.amber,
-                          padding: EdgeInsets.all(3.0),
-                          child: Row(
-                            crossAxisAlignment: CrossAxisAlignment.center,
-                            mainAxisAlignment: MainAxisAlignment.center,
-                            children: <Widget>[
-                              Icon(Icons.whatsapp),
-                              Text("Message",
-                                textAlign: TextAlign.center,
-                                style: TextStyle(
-                                  fontSize: 17,
-                                ),
-                              )
-                            ],
-                          )
-                        // icon: 'message',
-                        // child: Text("Send Message"),
-                      ),
-                      SizedBox(height: 140),
-                    ],
-                  ) ,
+      body: Center(
+        child: Column(
+          children: [
+            SizedBox(height: 50),
+            Container(
+              height: 140,
+              width: 140,
+              decoration: BoxDecoration(
+                image: DecorationImage(
+                  alignment: Alignment.center,
+                  image: AssetImage("assets/chat.png"),
                 ),
               ),
-              )
-            ],
-          ),
+            ),
+            Expanded (
+              child: Container(
+                  child: Column(
+                      children: <Widget>[
+                        SizedBox(height: 30),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: const [
+                            Expanded(
+                              child: Divider(
+                                indent: 20.0,
+                                endIndent: 10.0,
+                                thickness: 1,
+                              ),
+                            ),
+                            Text(
+                              "Send messages without saving contacts",
+                              style: TextStyle(color: Colors.blueGrey),
+                            ),
+                            Expanded(
+                              child: Divider(
+                                indent: 10.0,
+                                endIndent: 20.0,
+                                thickness: 1,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 10,),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(30, 20, 0, 2),
+                          child:
+                          Align(
+                              alignment: Alignment.centerLeft,
+                              child:Center(
+                                child:Text("Message Now!",
+                                  style: GoogleFonts.lato(textStyle: TextStyle(
+                                      fontSize: 22,
+                                      fontWeight: FontWeight.w700
+                                  )),
+                                ),
+                              )
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(40, 20, 40, 10),
+                          child:
+                          TextFormField(
+                            controller: number,
+                            keyboardType: TextInputType.phone,
+                            maxLength: 10,
+                            decoration: InputDecoration(
+                              focusedBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black, width: 1.0),
+                              ),
+                              enabledBorder: OutlineInputBorder(
+                                borderSide: BorderSide(color: Colors.black, width: 1.0),
+                              ),
+                              prefixIcon: Padding(
+                                  padding: EdgeInsets.all(15),
+                                  child: Text('+91 ')
+                              ),
+                              suffixIcon: IconButton(
+                                onPressed: (){
+                                  number.clear();
+                                },
+                                icon: Icon(
+                                  Icons.clear,
+                                  color: Colors.black,
+                                ),
+                              ),
+                              hintText: "WhatsApp Number",
+                            ),
+                          ),
+                        ),
+                        Container(
+                          padding: const EdgeInsets.fromLTRB(40, 2, 40, 10),
+                          child:
+                          RaisedButton(
+                              onPressed: () => _launchURLBrowser(number.text),
+                              color: Colors.amber,
+                              padding: EdgeInsets.all(3.0),
+                              child: Row(
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: <Widget>[
+                                  Icon(Icons.whatsapp),
+                                  Text("Message",
+                                    textAlign: TextAlign.center,
+                                    style: TextStyle(
+                                      fontSize: 17,
+                                    ),
+                                  )
+                                ],
+                              )
+                            // icon: 'message',
+                            // child: Text("Send Message"),
+                          ),
+                        ),
+                      ]
+                  )
+              ),
+            ),
+          ],
         ),
+      ),
         appBar: AppBar(
           title: Container(
               child: Row(
